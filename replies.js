5ae953ca325a265c921f386d98eb038665019762
C.S. Jiang <christine.s.jiang@gmail.com>
2017-01-22 23:42:48 -0500

    refactored API calls and added some (def not yet complete) help for when a tweet has an invalid geographic location associated

--- replies.js
+++ replies.js
@@ -8,64 +8,70 @@ const tenYearsAgo = moment().subtract(10, 'years').unix();
 
 const cities = require('./data/climacticCities'); 
 
-const weatherReply = incomingTweet => {
-	
-	// picks a random city to retrieve data for if the tweet being processed doesn't include location data
-	const place = incomingTweet.place ? incomingTweet.place : cities[Math.floor(Math.random() * cities.length)]; 
-	const locationQueryString = place.replace(', ', ',+');
-
-	const googleURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationQueryString}&key=${googleKey}`
+const googleAPICall = placename => {
+	const locationQueryString = placename.replace(/[-!#$%^&*()_+|~=`{}\[\]:";'<>?\s]/g, '').replace(',', ',+');
 	const googleOptions = {
-		uri: googleURL,
+		uri: `https://maps.googleapis.com/maps/api/geocode/json?address=${locationQueryString}&key=${googleKey}`,
 		headers: {
 				'User-Agent': 'Request-Promise'
 		},
 		json: true 
 	};
-
-	// gets longitude and latitude for location through google geocode API
-	return rp(googleOptions)
-	.then(response => response.results[0].geometry.location)
-	.then(({ lat, lng }) => {
-
-		// forecast from 10 years ago
-		const oldDataOptions = {
-			uri: `https://api.darksky.net/forecast/${darkSkyKey}/${lat},${lng},${tenYearsAgo}`,
-			headers: {
-					'User-Agent': 'Request-Promise'
-				},
-				json: true 
-			};  
-
-		// current forecast
-		const newDataOptions = {
-			uri: `https://api.darksky.net/forecast/${darkSkyKey}/${lat},${lng},${today}`,
-			headers: {
-					'User-Agent': 'Request-Promise'
-			},
-			json: true 
-		};
-				
-		return rp(oldDataOptions)
-		// calculates average temperature on given day from max + min temp
-		.then(data => ((data.daily.data[0].temperatureMin + data.daily.data[0].temperatureMax) / 2))
-		.then(prevTemp => rp(newDataOptions)
-			.then(data => ((data.daily.data[0].temperatureMin + data.daily.data[0].temperatureMax) / 2)) 
-			.then(newTemp => {
-
-				let pctChange = ((newTemp - prevTemp) / prevTemp * 100).toFixed(1)
-				pctChange = pctChange > 0 ? `+${pctChange}%` : `${pctChange}%`
-
-				const content = `Hi @${incomingTweet.screen_name}. Avg temp in ${place} 10 yrs ago was ${prevTemp.toFixed(1)}°F; today it is ${newTemp.toFixed(1)}°F. (${pctChange} change)`
-				return ({
-					tweetIdToReplyTo: incomingTweet.id,
-					content,
-				});
-			})
-		)
-	})
-	.catch(e => console.log(e));
+	return rp(googleOptions);
 };
+
+const getGoogleCoords = incomingTweet => {
+	// gets longitude and latitude for location through google geocode API. picks a random city to retrieve data for if the tweet being processed doesn't include location data
+
+	let place = incomingTweet.place 
+		? incomingTweet.place 
+		: cities[Math.floor(Math.random() * cities.length)]; 
+	return googleAPICall(place)
+	.then(response => new Promise((resolve, reject) => {
+			if (response.results.length) resolve(Object.assign({}, response.results[0].geometry.location, { place }));
+			else {
+				place = cities[Math.floor(Math.random() * cities.length)];
+				return googleAPICall(place)
+					.then(response => resolve(Object.assign({}, response.results[0].geometry.location, { place })))
+			}
+		})
+	)
+};
+
+// gets historical weather given a coordinates and a unix-formatted time
+const makeDarkSkyOptions = (lat, lng, date) => ({
+	uri: `https://api.darksky.net/forecast/${darkSkyKey}/${lat},${lng},${date}`,
+	headers: {
+		'User-Agent': 'Request-Promise'
+	},
+	json: true 
+});
+
+const weatherReply = incomingTweet => 
+	
+	getGoogleCoords(incomingTweet)
+	.then(({ lat, lng, place }) => 
+		//gets current forecast + forecast from 10y ago
+		Promise.all([
+			rp(makeDarkSkyOptions(lat, lng, tenYearsAgo)), 
+			rp(makeDarkSkyOptions(lat, lng, today))
+		])
+	// calculates average temperature on given day from max + min temp
+		.then(data => data.map(d => (d.daily.data[0].temperatureMin + d.daily.data[0].temperatureMax) / 2))
+		.then(([prevTemp, newTemp]) => ({
+			pctChange: ((newTemp - prevTemp) / prevTemp * 100).toFixed(1),
+			prevTemp, 
+			newTemp
+		}))
+		.then(({ prevTemp, newTemp, pctChange }) => {
+			pctChange = pctChange > 0 ? `+${pctChange}%` : `${pctChange}%`;
+			const content = `Hi @${incomingTweet.screen_name}. Avg temp in ${place} 10 yrs ago was ${prevTemp.toFixed(1)}°F; today it is ${newTemp.toFixed(1)}°F. (${pctChange} change)`
+			return ({
+				tweetIdToReplyTo: incomingTweet.id,
+				content,
+			});
+		}))
+	.catch(e => console.log(e));
 
 const infoReply = incomingTweet => {
 	var content = `Thanks for your interest in climate change, @${incomingTweet.screen_name}. For more information, please see http://bit.ly/2gecocA. Have a beautiful day!`;
